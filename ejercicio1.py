6ffe6dc359f000f06040fc59de38f80348016074
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-11 01:03:24 -0500

    se agrego el 1er ejercicio(15 indexacion)

---
+++ ejercicio1.py
@@ -0,0 +1,81 @@
+#ejemplos de indexacion:
+
+# ejemplo 1:
+#              1         2         3         4         5         6         7         8         9
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678
+msj="Al cerrar las persianas, el viento comenzó a soplar con más fuerza y escuchamos un ruido muy fuerte"
+#pedir la letra:
+a=msj[6]  # imprimir la letra r
+print(a)
+
+# ejemplo 2:
+b=msj[35]    #imprimir la letra c
+#pedir la letra
+print(b)
+
+# ejemplo 3:
+c=msj[20]    #imprimir la letra n
+#pedir la letra
+print(c)
+
+# ejemplo 4:
+d=msj[56]    #imprimir la letra m
+#pedir la letra
+print(d)
+
+# ejemplo 5:
+e=msj[47]    #imprimir la letra p
+#pedir la letra
+print(e)
+
+# ejemplo 6:
+f=msj[75]    #imprimir la letra a
+#pedir la letra
+print(f)
+
+# ejemplo 7:
+g=msj[15]    #imprimir la letra e
+#pedir la letra
+print(g)
+
+# ejemplo 8:
+h=msj[23]    #imprimir la letra ,
+#pedir la letra
+print(h)
+
+# ejemplo 9:
+i=msj[1]    #imprimir la letra l
+#pedir la letra
+print(i)
+
+# ejemplo 10:
+j=msj[95]    #imprimir la letra e
+#pedir la letra
+print(j)
+
+# ejemplo 11:
+k=msj[83]    #imprimir la letra r
+#pedir la letra
+print(k)
+
+# ejemplo 12:
+l=msj[89]    #imprimir la letra m
+#pedir la letra
+print(l)
+
+# ejemplo 13:
+m=msj[7]    #imprimir la letra a
+#pedir la letra
+print(m)
+
+# ejemplo 14:
+n=msj[11]    #imprimir la letra a
+#pedir la letra
+print(n)
+
+# ejemplo 15:
+o=msj[17]    #imprimir la letra s
+#pedir la letra
+print(o)
+
+#fin
---
+++ ejercicio2.py
@@ -0,0 +1,150 @@
+# ejemplos de longitud:
+
+# ejercicio 1:
+
+# mensaje:
+#               1         2         3         4         5         6
+#     0123456789012345678901234567890123456789012345678901234567890123456
+msj1="El diario publicó una nota incorrecta que el editor había prohibido"
+
+# impresion
+print(len(msj1))        # longitud=67
+
+# ejercicio 2:
+
+# mensaje:
+#               1         2         3         4
+#     0123456789012345678901234567890123456789012345
+msj2="La casa estaba limpia y las cortinas brillaban"
+
+# impresion
+print(len(msj2))        # longitud=46
+
+# ejercicio 3:
+
+# mensaje:
+#               1         2         3
+#     0123456789012345678901234567890123456
+msj3="HOLA COMO ESTAN , ¿TODO BIEN EN CASA?"
+
+# impresion
+print(len(msj3))        # longitud=37
+
+# ejercicio 4:
+
+# mensaje:
+#               1         2         3
+#     012345678901234567890123456789012345
+msj4="LA UNIVERSIDAD , MI CASA DE ESTUDIOS"
+
+# impresion
+print(len(msj4))        # longitud=36
+
+# ejercicio 5:
+
+# mensaje:
+#               1         2
+#     01234567890123456789012345
+msj5="NOS VEMOS LA PROXIMA CLASE"
+
+# impresion
+print(len(msj5))        # longitud=26
+
+# ejercicio 6:
+
+# mensaje:
+#               1         2
+#     0123456789012345678901
+msj6="INGENIERIA ELECTRONICA"
+
+# impresion
+print(len(msj6))        # longitud=22
+
+# ejercicio 7:
+
+# mensaje:
+#               1
+#     01234567890123456
+msj7="HOLAAAAAAAAAAAAAA"
+
+# impresion
+print(len(msj7))        # longitud=17
+
+# ejercicio 8:
+
+# mensaje:
+#              1
+#     012345678901
+msj8="JORGE ANDREE"
+
+# impresion
+print(len(msj8))        # longitud=12
+
+# ejercicio 9:
+
+# mensaje:
+
+#     0123456789
+msj9="HOLA MUNDO"
+
+# impresion
+print(len(msj9))        # longitud=10
+
+# ejercicio 10:
+
+# mensaje:
+#                1
+#      01234567890123456
+msj10="SIEMPRE ESTUDIOSO"
+
+# impresion
+print(len(msj10))        # longitud=17
+
+# ejercicio 11:
+
+# mensaje:
+
+#      0123
+msj11="baby"
+
+# impresion
+print(len(msj11))        # longitud=4
+
+# ejercicio 12:
+
+# mensaje:
+#               1
+#     01234567890
+msj12="UNIVERSIDAD"
+
+# impresion
+print(len(msj12))        # longitud=11
+
+# ejercicio 13:
+
+# mensaje:
+#     0123456
+msj13="FAMILIA"
+
+# impresion
+print(len(msj13))        # longitud=7
+
+# ejercicio 14:
+
+# mensaje:
+#               1
+#     012345678901234
+msj4="MENSAJE CATORCE"
+
+# impresion
+print(len(msj4))        # longitud=15
+
+# ejercicio 15:
+
+# mensaje:
+#                1         2         3
+#      012345678901234567890123456789012345
+msj15="UNIVERSIDAD NACIONAL PEDRO RUZ GALLO"
+
+# impresion
+print(len(msj15))        # longitud=36
