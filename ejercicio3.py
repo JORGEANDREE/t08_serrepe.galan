cc3c6afadaed28c66f9dc2aac0476476dddf76a3
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-12 13:56:20 -0500

    se agrego el 3er ejercicio(15 comparacion)

---
+++ ejercicio3.py
@@ -0,0 +1,106 @@
+# EJERCICIO DE COMPARACION
+
+# ejercicio 1
+# pedir variables
+msj1="HOLA RODRIGO"
+msj2="MUNDO"
+#imprimir comparcion
+print(msj1==msj2)  # false
+
+# ejercicio 2
+# pedir variables
+msj3="antonio"
+msj4="JORGE"
+#imprimir comparcion
+print(msj3!=msj4)  # True
+
+# ejercicio 3
+# pedir variables
+msj5="CARPETA"
+msj6="dinosaurio"
+#imprimir comparcion
+print(msj5<=msj6)  # True
+
+# ejercicio 4
+# pedir variables
+msj7="planeta"
+msj8="carro"
+#imprimir comparcion
+print(msj7>=msj8)  # True
+
+# ejercicio 5
+# pedir variables
+msj9="k"
+msj10="L"
+#imprimir comparcion
+print(msj9==msj10)  # false
+
+# ejercicio 6
+# pedir variables
+msj11="a"
+msj12="A"
+#imprimir comparcion
+print(msj11!=msj12)  # True
+
+# ejercicio 7
+# pedir variables
+msj13="ropa"
+msj14="ROPA"
+#imprimir comparcion
+print(msj13==msj14)  # false
+
+# ejercicio 8
+# pedir variables
+msj15="Hola"
+msj16="MUNDO"
+#imprimir comparcion
+print(msj15<=msj16)  # True
+
+# ejercicio 9
+# pedir variables
+msj17="nomo"
+msj18="robo"
+#imprimir comparcion
+print(msj17!=msj18)  # True
+
+# ejercicio 10
+# pedir variables
+msj19="gato"
+msj20="perro"
+#imprimir comparcion
+print(msj19>=msj20)  # false
+
+# ejercicio 11
+# pedir variables
+msj21="fernando"
+msj22="profesor"
+#imprimir comparcion
+print(msj21>=msj22)  # false
+
+# ejercicio 12
+# pedir variables
+msj23="lapiz"
+msj24="PIZZA"
+#imprimir comparcion
+print(msj23==msj24)  # false
+
+# ejercicio 13
+# pedir variables
+msj25="loro"
+msj26="Q"
+#imprimir comparcion
+print(msj25!=msj26)  # True
+
+# ejercicio 14
+# pedir variables
+msj27="SERREPE"
+msj28="SANTISTEBAN"
+#imprimir comparcion
+print(msj27==msj28)  # false
+
+# ejercicio 15
+# pedir variables
+msj29="ramon"
+msj30="florinda"
+#imprimir comparcion
+print(msj29!=msj30)  # True
