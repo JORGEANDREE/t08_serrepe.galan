03024bfb3d2c84536277f271921434ff9f0cd1e2
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-12 14:14:18 -0500

    se agrego el 4to ejercicio(15 concatenacion)

---
+++ ejercicio4.py
@@ -0,0 +1,114 @@
+# EJERCICIOS DE CONCATENACION
+
+# ejercicio 1
+# pedir variables
+msj1="HOLA"
+msj2="MUNDO"
+#imprimir concatenacion
+print(msj1+" "+msj2)  # HOLA MUNDO
+
+# ejercicio 2
+# pedir variables
+msj3="TE "
+msj4="AMO"
+#imprimir concatenacion
+print(msj3+msj4)  # TE AMO
+
+# ejercicio 3
+# pedir variables
+msj5="CARPETA"
+msj6="VACIA"
+#imprimir concatenacion
+print(msj5+" "+msj6)  # CARPETA VACIA
+
+# ejercicio 4
+# pedir variables
+msj7="planeta"
+msj8="limpio"
+#imprimir concatenacion
+print(msj7+"\n"+msj8)  # pleneta
+                       #limpio
+
+# ejercicio 5
+# pedir variables
+msj9="LA"
+msj10="CANCION"
+#imprimir concatenacion
+print(msj9+" "+ msj10)  # LA CANCION
+
+# ejercicio 6
+# pedir variables
+msj11="rosa lava"
+msj12="la ropa"
+#imprimir concatenacion
+print(msj11+"\n"+msj12)  # rosa lava
+                         # la ropa
+
+# ejercicio 7
+# pedir variables
+msj13="la ropa "
+msj14="esta limpia"
+#imprimir concatenacion
+print(msj13+msj14)  # la ropa esta limpia
+
+# ejercicio 8
+# pedir variables
+msj15="mundo "
+msj16="cruel"
+#imprimir concatenacion
+print(msj15+msj16)  # mundo cruel
+
+# ejercicio 9
+# pedir variables
+msj17="perro"
+msj18="fiel"
+#imprimir concatenacion
+print(msj17+" "+msj18)  # perro fiel
+
+# ejercicio 10
+# pedir variables
+msj19="el perro es:""\n"
+msj20="el mejor amigo del hombre"
+#imprimir concatenacion
+print(msj19+msj20)  # el perro es:
+                    # el mejor amigo del hombre
+
+# ejercicio 11
+# pedir variables
+msj21="fernando es el "
+msj22="profesor de programacion"
+#imprimir concatenacion
+print(msj21+msj22)  # fernando es el profesor de programacion
+
+# ejercicio 12
+# pedir variables
+msj23="no se permiten""\n"
+msj24="ANIMALES"
+#imprimir concatenacion
+print(msj23+msj24)  # no se permiten
+                    # ANIMALES
+
+# ejercicio 13
+# pedir variables
+msj25="adrian comio toda"
+msj26="su comida"
+#imprimir concatenacion
+print(msj25+" "+msj26)  # adrian comio toda su comida
+
+# ejercicio 14
+# pedir variables
+msj27="JORGE ANDREE""\n"
+msj28="SERREPE SANTISTEBAN"
+#imprimir concatenacion
+print(msj27+msj28)  # JORGE ANDREE
+                    # SERREPE SANTISTEBAN
+
+# ejercicio 15
+# pedir variables
+msj29="H"
+msj30="O"
+msj31="L"
+msj32="A"
+#imprimir comparcion
+print(msj29+msj30+msj31+msj32)  # HOLA
+
