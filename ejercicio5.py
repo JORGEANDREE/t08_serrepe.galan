2cfbcb10d5e7d6fdf384ddeb5931ab68dc7baa5c
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-12 23:16:33 -0500

    se agrego el 5to ejercicio(15 cortado de cadenas)

---
+++ ejercicio5.py
@@ -0,0 +1,155 @@
+# EJERCICIOS DE CORTADO DE CADENAS
+
+# ejercicio1:
+#pedir variable
+#              1
+#    01234567890123456789
+msj="HOLA ME LLAMO ANDREE"
+#    98765432109876543210-
+#             1
+#imprimir cortado de cadenas
+print("el msj es:",msj[0:4]) #HOLA
+
+# ejercicio2:
+#pedir variable
+
+#              1         2         3
+#    012345678901234567890123456789012345678
+msj="FERNANDO ES EL PROFESOR DE PROGRAMACION"
+#    876543210987654321098765432109876543210-
+#            3         2         1
+
+#imprimir cortado de cadenas
+print("el msj es:",msj[-0:-12]) # FERNANDO ES EL PROFESOR DE
+
+# ejercicio3:
+#pedir variable
+#              1
+#    01234567890123456789
+msj="EL CARRO ESTA LIMPIO"
+#    98765432109876543210-
+#             1
+
+#imprimir cortado de cadenas
+print("el msj es:",msj[14:])    # LIMPIO
+
+# ejercicio4:
+#pedir variable
+#              1         2         3
+#    0123456789012345678901234567890123
+msj="EL PERRO EL MEJOR AMIGO DEL HOMBRE"
+#    3210987654321098765432109876543210-
+#       3         2         1
+
+#imprimir cortado de cadenas
+print("el msj es:",msj[3:8]) # PERRO
+
+# ejercicio5:
+#pedir variable
+#              1
+#    01234567890123456
+msj="SIEMPRE JUNTOS :v"
+#    65432109876543210-
+#          1
+
+#imprimir cortado de cadenas
+print("el msj es:",msj[15:])   #:v
+
+# ejercicio6:
+#pedir variable
+#              1
+#    0123456789012345678
+msj="ERES EL MEJOR AMIGO"
+#    8765432109876543210-
+#            1
+#imprimir cortado de cadenas
+print("el msj es:",msj[0:2])   #ER
+
+# ejercicio8:
+#pedir variable
+#              1        2
+#    01234567890123456890
+msj="ROSA DUERME TEMPRANO"
+#    098765432109876543210-
+#    2         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[0:4])   #:v
+
+# ejercicio8:
+#pedir variable
+#              1         2         3
+#    01234567890123456789012345678901
+msj="JORGE ANDREE SERREPE SANTISTEBAN"
+#    10987654321098765432109876543210-
+#     3         2         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[4::-1])   #EGROJ
+
+# ejercicio9:
+#pedir variable
+#              1
+#    0123456789012345
+msj="gracias por todo"
+#    5432109876543210-
+#         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[6::-1])   #saicarg
+
+# ejercicio10:
+#pedir variable
+#              1
+#    012345678901234
+msj="VAMOS AL PARQUE"
+#    432109876543210-
+#        1
+#imprimir cortado de cadenas
+print("el msj es:",msj[2::-1])   # MAV
+
+# ejercicio11:
+#pedir variable
+#              1         2
+#    012345678901234567890
+msj="EL CARRO ES DE ANDREE"
+#    098765432109876543210-
+#    2         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[0:])   # EL CARRO ES DE ANDREE
+
+# ejercicio12:
+#pedir variable
+#              1         2         3
+#    0123456789012345678901234567890123
+msj="EL DINOSAURIO ES UN ANIMAL EXTINTO"
+#    3210987654321098765432109876543210-
+#       3         2         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[33:26:-1])   # OTNITXE
+
+# ejercicio13:
+#pedir variable
+#    0123
+msj="AMOR"
+#    3210-
+#imprimir cortado de cadenas
+print("el msj es:",msj[::-1])   # ROMA
+
+# ejercici14:
+#pedir variable
+#              1         2
+#    012345678901234567890123
+msj="juan salio a comprar pan"
+#    321098765432109876543210-
+#       3         2         1
+#imprimir cortado de cadenas
+print("el msj es:",msj[23::-1])   #nap rarpomc a oilas nauj
+
+# ejercicio15:
+#pedir variable
+#              1
+#    0123456789012345678
+msj="rosa salio a bailar"
+#    8765432109876543210-
+#            1
+#imprimir cortado de cadenas
+print("el msj es:",msj[::4])   # r i l
+
