3530af7b89d7f7a85d93d5f8df36cdf818729783
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-12 23:54:52 -0500

    se agrego el 7mo ejercicio(15 de busqueda)

---
+++ ejercicio7.py
@@ -0,0 +1,107 @@
+# EJERCICIO DE BUSQUEDA
+
+# ejercicio1:
+# pedir variables
+msj="PABLITO CLAVO UN CLAVITO EN LA CALVA DE UN CALVITO"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("PABLITO"))  #0
+print("fin del 1er ejercicio")
+
+# ejercicio2:
+# pedir variables
+msj="SOLOS TU Y YO"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("TU"))  #6
+print("fin del 2do ejercicio")
+
+# ejercicio3:
+# pedir variables
+msj="navidad en familia"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("familia"))  #11
+print("fin del 3er ejercicio")
+
+# ejercicio4:
+# pedir variables
+msj="siempre seras tu"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("ras"))  #10
+print("fin del 4to ejercicio")
+
+# ejercicio5:
+# pedir variables
+msj="el perro se comio tu comida"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("comio"))  #12
+print("fin del 5to ejercicio")
+
+# ejercicio6:
+# pedir variables
+msj="EL CARRO SE DESCOMPUSO"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.index("CARRO"))  #3
+print("fin del 6to ejercicio")
+
+# ejercicio7:
+# pedir variables
+msj="EL LORO PEPE"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.index("E"))  #0
+print("fin del 7mo ejercicio")
+
+# ejercicio8:
+# pedir variables
+msj="SOFIA SE ENOJO"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.index("ENOJO"))  #9
+print("fin del 8vo ejercicio")
+
+# ejercicio9:
+# pedir variables
+msj="julia compro un auto nuevo"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.index("u"))  #1
+print("fin del 9no ejercicio")
+
+# ejercicio10:
+# pedir variables
+msj="monica se arreglo el cabello"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.index("cabello"))  #21
+print("fin del 10mo ejercicio")
+
+# ejercicio11:
+# pedir variables
+msj="fatima es amable con los demas"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("maritza"))  #-1
+print("fin del 11vo ejercicio")
+
+# ejercicio12:
+# pedir variables
+msj="EL PERRO JUEGA CON LA PELOTA"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("GATO"))  #-1
+print("fin del 12vo ejercicio")
+
+
+# ejercicio13:
+# pedir variables
+msj="jlisd"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("x"))  #-1
+print("fin del 13vo ejercicio")
+
+# ejercicio14:
+# pedir variables
+msj="DIOS TE AMA"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("AMA"))  #8
+print("fin del 14vo ejercicio")
+
+# ejercicio15:
+# pedir variables
+msj="TOSO APROVAMOS EL CURSO DE PROGRAMACION"
+#IMPRIMIR LA OPERACION BUSQUEDA
+print(msj.find("PROGRAMACION"))  #27
+print("fin del 6to ejercicio")
