1d0c516240224587546a6f15c1bd659b3fbcef87
JEFFRYDANIEL <jgalanasenmache@gmail.com>
2019-12-13 13:14:13 -0500

    se agrego los 15 ejercicios de concatenacion

---
+++ galan/concatenacion.py
@@ -0,0 +1,104 @@
+#EJERCICIO1:
+#MOSTRAR "NO QUIERO IR AL CINE" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123
+msg="NO ES POBRE EL QUE TIENE POCO, SINO EL QUE MUCHO DESEA"
+print(msg[0]+msg[1] + " " + msg[39]+msg[40]+msg[32]+msg[50]+msg[9]+msg[47] + " "+ msg[32]+msg[9] + " " + msg[53]+msg[37]+ " " + msg[27]+msg[20]+msg[33]+msg[21])
+
+#EJERCICIO12:
+#MOSTRAR "MI ABUELO ES MI MAESTRO" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123456789
+msg="NO SIEMPRE ES LA MULTITUD LA POSEEDORA DE LA VERDAD ABSOLUTA"
+print(msg[6]+msg[4]+ " " + msg[52]+msg[53]+msg[57]+msg[46]+msg[26]+msg[35] + " " + msg[11]+msg[8]+msg[52] + " " + msg[17]+msg[4] + " " + msg[17]+msg[37]+msg[11]+msg[12]+msg[58]+msg[8]+msg[55])
+
+#EJERCICIO3:
+#MOSTRAR "VIVIR LA VIDA" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="VIVE COMO SI FUERAS A MORIR MAÑANA; APRENDE COMO SI EL MUNDO FUERA A DURAR PARA SIEMPRE"
+print(msg[0]+msg[1]+msg[2]+msg[1]+msg[38] + " " + msg[53]+msg[36] + " " + msg[0]+msg[1]+msg[41]+msg[17])
+
+#EJERCICIO4:
+#MOSTRAR "PRIMERO SUPRIME" DE LA SIGUIENTE CADENA:
+#              1         2
+#    012345678901234567890123456
+msg="MUCHO PRESUMIR Y POCO VALER"
+print(msg[6]+msg[7]+msg[12]+msg[11]+msg[8]+msg[26]+msg[18] + " " + msg[9]+msg[10]+msg[6]+msg[7]+msg[12]+msg[11]+msg[25])
+
+#EJERCICIO5:
+#MOSTRAR "PUEDES IR A TU CASA" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7         8         9
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+msg="CUANDO TODO PARECE IR MAL, PASAN COSAS BUENAS QUE NO HABRIA OCURRIDO SI TODO HUBIERA FUNCIONADO"
+print(msg[12]+msg[1]+msg[41]+msg[66]+msg[41]+msg[44] + " " + msg[70]+msg[20] + " " + msg[23] + " " + msg[72]+msg[78] + " " + msg[33]+msg[28]+msg[29]+msg[58])
+
+#EJERCICIO6:
+#MOSTRAR "COCINA CON AMOR" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6
+#    012345678901234567890123456789012345678901234567890123456789012
+msg="EN EL CAMINO PERDERAS PERSONAS Y COSAS. NO TE PIERDAS CON ELLAS"
+print(msg[6]+msg[11]+msg[33]+msg[47]+msg[40]+msg[7] + " " + msg[33]+msg[34]+msg[56] + " " + msg[7]+msg[8]+msg[55]+msg[49])
+
+#EJERCICIO7:
+#MOSTRAR "NO LE TEMAS A NADIE" DE LA SIGUIENTE CADENA:
+#              1         2         3         4
+#    0123456789012345678901234567890123456789012345
+msg="LA VIDA ES MARAVILLOSA SI NO SE LE TIENE MIEDO"
+print(msg[26]+msg[27] + " " + msg[32]+msg[33] + " " + msg[35]+msg[39]+msg[41]+msg[1]+msg[23]+ " " + msg[1] + " " + msg[26]+msg[6]+msg[44]+msg[36]+msg[39])
+
+#EJERCICIO8:
+#MOSTRAR "PIENSA EN TI" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7         8         9        10
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+msg="SI NO CREEMOS EN LA LIBERTAD DE EXPRESIÓN PARA LA GENTE QUE DESPRECIAMOS, NO CREEMOS EN ELLA EN ABSOLUTO"
+print(msg[42]+msg[67]+msg[8]+msg[74]+msg[0]+msg[18] + " " + msg[51]+msg[74] + " " + msg[25]+msg[21])
+
+#EJERCICIO9:
+#MOSTRAR "NO PERMANECE EN SU LUGAR" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789
+msg="NO BUSQUES PERSONAS CON TUS MISMOS GUSTOS, BUSCA PERSONAS CON TUS MISMOS VALORES"
+print(msg[0]+msg[1] + " " + msg[11]+msg[12]+msg[51]+msg[66]+msg[55]+msg[16]+msg[12]+msg[20]+msg[8] + " " + msg[12]+msg[0] + " " + msg[18]+msg[36] + " " + msg[75]+msg[63]+msg[35]+msg[74]+msg[77])
+
+#EJERCICIO10:
+#MOSTRAR "NUNCA LE RUEGUES A ALGUIEN" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6
+#    012345678901234567890123456789012345678901234567890123456789012
+msg="LO PEOR QUE HACEN LOS MALOS ES OBLIGARNOS A DUDAR DE LOS BUENOS"
+print(msg[16]+msg[45]+msg[16]+msg[14]+msg[13] + " " + msg[53]+msg[59] + " " + msg[37]+msg[58]+msg[4]+msg[35]+msg[45]+msg[28]+msg[62] + " " + msg[42] + " " +msg[47]+msg[33]+msg[35]+msg[9]+msg[34]+msg[59]+msg[38])
+
+#EJERCICIO11:
+#MOSTRAR "LEE CON CALMA" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+msg="LOCURA ES HACER LA MISMA COSA UNA Y OTRA VEZ ESPERANDO OBTENER DIFERENTES RESULTADOS"
+print(msg[0]+msg[42]+msg[42] + " " + msg[25]+msg[53]+msg[51] + " " + msg[25]+msg[5]+msg[78]+msg[19]+msg[32])
+
+#EJERCICIO12:
+#MOSTRAR "CUIDATE BIEN" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7         8         9
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+msg="CUANDO TODO PARECE IR MAL, PASAN COSAS BUENAS QUE NO HABRIA OCURRIDO SI TODO HUBIERA FUNCIONADO"
+print(msg[33]+msg[1]+msg[70]+msg[66]+msg[43]+msg[72]+msg[81] + " " + msg[39]+msg[65]+msg[81]+msg[91])
+
+#EJERCICIO13:
+#MOSTRAR "RIETE DE ALGO" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678
+msg="EL RITMO ES ALGO QUE SE TIENE O NO TIENE, PERO CUANDO LO TIENES, LO TIENES TODO"
+print(msg[3]+msg[22]+msg[9]+msg[24]+msg[39] + " " + msg[77]+msg[59] + " " + msg[12]+msg[13]+msg[14]+msg[15])
+
+#EJERCICIO14:
+#MOSTRAR "JUNTA TUS COSAS" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6
+#    012345678901234567890123456789012345678901234567890123456789012345
+msg="LA PEREZA VIAJA TAN DESPACIO QUE LA POBREZA NO TARDA EN ALCANZARLA"
+print(msg[13]+msg[30]+msg[44]+msg[47]+msg[56] + " " + msg[47]+msg[30]+msg[22] + " " + msg[25]+msg[45]+msg[22]+msg[51]+msg[22])
+
+#EJERCICIO15:
+#MOSTRAR "NADA ES PARA SIEMPRE" DE LA SIGUIENTE CADENA:
+#              1         2         3         4         5         6         7
+#    0123456789012345678901234567890123456789012345678901234567890123456789012
+msg="NO PERMITAS QUE NINGUN SER HUMANO TE HAGA CAER TAN BAJO COMO PARA ODIARLE"
+print(msg[16]+msg[9]+msg[67]+msg[9] + " " + msg[44]+msg[10] + " " + msg[3]+msg[9]+msg[70]+msg[9] + " " + msg[10]+msg[68]+msg[72]+msg[6]+msg[3]+msg[45]+msg[72])
