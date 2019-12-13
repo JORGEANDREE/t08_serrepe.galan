fe0553cdd8bd2a7c881e3ff9dea26d51a8637d2d
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-12-12 23:31:37 -0500

    se agrego el 6to ejercicio(15 de iteracion)

---
+++ ejercicio6.py
@@ -0,0 +1,166 @@
+# EJERCICIO DE ITERACION:
+
+# ejercicio1:
+# pedir variables
+nombre="JUAN"
+# impresion
+print("el primer msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 1ER MSJ")
+
+# ejercicio2:
+# pedir variables
+nombre="rosa"
+# impresion
+print("el segundo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 2DO MSJ")
+
+# ejercicio3:
+# pedir variables
+nombre="HOLA MUNDO"
+# impresion
+print("el tercer msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 3ER MSJ")
+
+# ejercicio4:
+# pedir variables
+nombre="DIOS TE AMA"
+# impresion
+print("el cuarto msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 4TO MSJ")
+
+# ejercicio5:
+# pedir variables
+nombre="SANDIA"
+# impresion
+print("el primer msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 5TO MSJ")
+
+# ejercicio6:
+# pedir variables
+nombre="PERRO"
+# impresion
+print("el sexto msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 6TO MSJ")
+
+# ejercicio7:
+# pedir variables
+nombre="IREMOS A NARNIA"
+# impresion
+print("el septimo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 7MO MSJ")
+
+# ejercicio8:
+# pedir variables
+nombre="GATO"
+# impresion
+print("el primer msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 8VO MSJ")
+
+# ejercicio9:
+# pedir variables
+nombre="KOALA"
+# impresion
+print("el noveno msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 9NO MSJ")
+
+# ejercicio10:
+# pedir variables
+nombre="LA FAMILIA ES MUY UNIDA"
+# impresion
+print("el decimo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 10MO MSJ")
+
+# ejercicio11:
+# pedir variables
+nombre="CARRO"
+# impresion
+print("el onceavo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 11VO MSJ")
+
+# ejercicio12:
+# pedir variables
+nombre="REBELION"
+# impresion
+print("el doceavo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 12VO MSJ")
+
+# ejercicio13:
+# pedir variables
+nombre="JORGE ANDREE"
+# impresion
+print("el treceavo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 13VO MSJ")
+
+# ejercicio14:
+# pedir variables
+nombre="SERREPE"
+# impresion
+print("el catorceavo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 14VO MSJ")
+
+# ejercicio15:
+# pedir variables
+nombre="SANTISTEBAN"
+# impresion
+print("el quinceavo msj es:")
+# sentencia for
+for letra in nombre:
+#impresion
+    print(letra)
+print("FIN DEL 15VO MSJ")
